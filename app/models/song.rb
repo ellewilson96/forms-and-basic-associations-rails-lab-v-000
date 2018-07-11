cd1c46dc1e654664ff940ae4b71aefd40b2916c5
Elle Wilson <eleanorwilsonn@gmail.com>
2018-07-11 23:01:22 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,12 +3,12 @@ class Song < ActiveRecord::Base
   belongs_to :genre
   has_many :notes
 
-  def self.artist_name
+  def artist_name
     @artist = self.artist.name
     @artist
   end
 
-  def self.genre_name
+  def genre_name
     @genre = self.genre.name
     @genre
   end
